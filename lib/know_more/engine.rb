e2900b502a54f26f88e066c24ec1f27dc83594c9
Julien Lu <julien@ekohe.com>
2016-11-06 20:47:01 +0800

    automatically add mountable route

--- lib/know_more/engine.rb
+++ lib/know_more/engine.rb
@@ -6,5 +6,11 @@ module KnowMore
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
+    initializer 'know_more.route' do |app|
+      app.routes.append do
+        mount KnowMore::Engine => "/know_more", as: 'know_more'
+      end
+    end
   end
 end
--- spec/test_app/config/routes.rb
+++ spec/test_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount KnowMore::Engine => "/know_more", as: 'know_more'
+  # mount KnowMore::Engine => "/know_more", as: 'know_more'
 
   root to: 'welcome#hello'
 end
